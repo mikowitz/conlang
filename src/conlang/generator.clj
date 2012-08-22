8ea4e493451367df807e3098e8c7caea422fcf8e
Michael Berkowitz <michael.berkowitz@gmail.com>
2012-08-22 18:24:45 -0400

    Add word generator.

--- src/conlang/generator.clj
+++ src/conlang/generator.clj
@@ -4,3 +4,7 @@
   (let [syl (rest (clojure.string/split (rand-nth syls) #""))]
     (apply str (map #(rand-nth ((keyword %) phonemes)) syl))
 ))
+
+(defn generate-word [syls phonemes]
+  (apply str (take (-> 4 rand-int inc)
+                   (repeatedly #(generate-syllable syls phonemes)))))
--- test/conlang/generator_test.clj
+++ test/conlang/generator_test.clj
@@ -2,6 +2,11 @@
   (:use clojure.test
         conlang.generator))
 
-(deftest basic-generator-test
+(deftest basic-syllable-generator-test
   (testing 
     (is (= "bab" (generate-syllable ["cvc"] {:c ["b"] :v ["a"]})))))
+
+(deftest basic-word-generator-test
+  (testing
+    (is (some #(= (generate-word ["cvc"] {:c ["b"] :v ["a"]}) %)
+              	  (map #(apply str (take % (repeat "bab"))) (range 1 5))))))
